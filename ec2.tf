ad36dbf546bea9fa37009aed446c821e88b357b9
Craig Ballingall <craig.ballingall@gmail.com>
2022-05-08 23:22:21 +0100

    Added windows host
    still needs manual service account and google logging agent

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,8 @@ resource "google_compute_instance" "my-logged-instance" {
   machine_type = "e2-micro"
   zone         = "europe-west2-a"
 
+  allow_stopping_for_update = true
+
   boot_disk {
     initialize_params {
       image = "debian-cloud/debian-9"
@@ -15,4 +17,32 @@ resource "google_compute_instance" "my-logged-instance" {
     access_config {
     }
   }
+
+}
+
+resource "google_compute_instance" "windows_logtest" {
+  name         = "windows-logtest"
+  
+  # rmO*?VEh)T%$NN;
+
+  machine_type = var.windows_instance_type
+  zone         = "europe-west2-a"
+  hostname     = "windows-log.example.com"
+  tags         = ["rdp", "http"]
+
+  allow_stopping_for_update = true
+
+  boot_disk {
+    initialize_params {
+      image = var.windows_2022_sku
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+
+    }
+  }
+ 
 }
